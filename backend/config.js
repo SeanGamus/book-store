ab2599e7f24672abfbe61c6073daaaab3ad28283
Aditya Singh <aditya.singh@okiedokiepay.com>
2023-11-20 15:10:32 +0530

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb://root:root@ac-0d70vrd-shard-00-00.wmyevzz.mongodb.net:27017,ac-0d70vrd-shard-00-01.wmyevzz.mongodb.net:27017,ac-0d70vrd-shard-00-02.wmyevzz.mongodb.net:27017/?ssl=true&replicaSet=atlas-xe0hgb-shard-0&authSource=admin&retryWrites=true&w=majority";
